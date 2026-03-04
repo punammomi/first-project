76c8775ba0b9efbf80e16d37df32ad061e1708b8
Punam <159411425+punammomi@users.noreply.github.com>
2026-03-04 22:10:18 +0600

    Create a new file name Js and here just use js variable data type, condition etc

---
+++ Js/index.js
@@ -0,0 +1,11 @@
+/*const prompt = require("prompt-sync")()
+const v = prompt("Type something:")
+console.log(v)*/
+
+const x ="3345.78"
+const y = 3
+
+console.log(parseFloat(x) + y)
+
+const cond = 3 < 3 ? "Okay" : "No"
+console.log(cond)
